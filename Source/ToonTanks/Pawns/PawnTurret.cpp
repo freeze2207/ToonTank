e2d3a18f36eda86b4a1daedfc42b7fbb99582091
Zihe Zhang <zhangzihezealf@gmail.com>
2021-08-18 17:15:44 -0400

    Basic movement

---
+++ Source/ToonTanks/Pawns/PawnTurret.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PawnTurret.h"
+#include "Kismet/GameplayStatics.h"
+#include "PawnTank.h"
+
+// Called when the game starts or when spawned
+void APawnTurret::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorld()->GetTimerManager().SetTimer(FireRateTimerHandle, this, &APawnTurret::CheckFireCondition, FireRate, true);
+
+	PlayerPawn = Cast<APawnTank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
+
+// Called every frame
+void APawnTurret::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APawnTurret::CheckFireCondition()
+{
+	// Player != null and Alive
+	if (!PlayerPawn)
+	{
+		return;
+	}
+	// In range then fire
+	if (ReturnDistanceToPlayer() <= FireRange)
+	{
+		// Fire
+		UE_LOG(LogTemp, Warning, TEXT("Fired"));
+
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Fire Condition checked"));
+}
+
+float APawnTurret::ReturnDistanceToPlayer()
+{
+	if (!PlayerPawn)
+	{
+		return 0.0f;
+	}
+
+	return FVector::Dist(PlayerPawn->GetActorLocation(), GetActorLocation());
+}
---
+++ Source/ToonTanks/Pawns/PawnTurret.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PawnBase.h"
+#include "PawnTurret.generated.h"
+
+
+class APawnTank;
+/**
+ * 
+ */
+
+UCLASS()
+class TOONTANKS_API APawnTurret : public APawnBase
+{
+	GENERATED_BODY()
+	
+public:
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+	APawnTank* PlayerPawn;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	float FireRate = 2.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	float FireRange = 500.0f;
+
+	FTimerHandle FireRateTimerHandle;
+
+	void CheckFireCondition();
+
+	float ReturnDistanceToPlayer();
+
+};
